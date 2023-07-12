6d7c7e94e32d217b5fca8871171d8d0713b501ce
DJ <epicmrjones@gmail.com>
2023-07-12 10:25:17 -0500

    New Class
    who dis

---
+++ Class4Review/MySpace/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Class4Review.MySpace
+{
+    public class Class1
+    {
+        public string[] students { get; set; }
+        public Class1()
+        {
+            students = new string[19];
+        }
+
+        public void LockIn() 
+        {
+            Console.WriteLine("{0} students are Locked In", students.Length);
+        }
+        public static void LockIn(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public class Date
+    {
+        private int _month = 7;  // Backing store
+
+        public int Month
+        {
+            get => _month;
+            set
+            {
+                if ((value > 0) && (value < 13))
+                {
+                    _month = value;
+                }
+            }
+        }
+    }
+}
+
+//const pizza = { }
+//const dylansPizza = new Pizza();
+
--- Class4Review/Program.cs
+++ Class4Review/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Class4Review
+﻿using System.Globalization;
+
+namespace Class4Review
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            MySpace.Class1 MyClass = new MySpace.Class1();
+            //MySpace.Class1.LockIn("I'm Locked In! 👉🏾"); // static method
+            //MyClass.LockIn(); // non static method
+            Console.Write(MyClass.students.Length);
+            Console.Write("\n");
+            MyClass.students[0] = "Jay";
+            Console.Write("Student #1 is " + MyClass.students[0]);
+
+            //MySpace.Date todaysDate = new MySpace.Date();
+            //Console.WriteLine(todaysDate.Month);
+            //todaysDate.Month = 8;
+            //Console.WriteLine(todaysDate.Month);
         }
     }
 }
