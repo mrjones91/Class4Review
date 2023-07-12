9839b029be33b492e6805777177e631b7b83dbf2
DJ <epicmrjones@gmail.com>
2023-07-12 09:40:35 -0500

    init

---
+++ Class4Review/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class4Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
